efe95e694eddee468c62dfdebf998bff31f0351a
Rafa Soyer <rcsoyer@gmail.com>
2024-04-20 14:42:11 +0200

    make the mapping bidirectional to make it easier to implement the register of the account which will contain all the data

--- src/main/java/org/acme/accountregistry/domain/Account.java
+++ src/main/java/org/acme/accountregistry/domain/Account.java
@@ -5,7 +5,12 @@ import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.LAZY;
 
 /**
  * A person's Account details with the bank.
@@ -15,15 +20,21 @@ import lombok.Getter;
 @Entity
 public class Account extends AbstractIdentityEntity {
 
+    @NotBlank(message = "A person's Name is mandatory")
     private String name;
 
+    @NotBlank(message = "A person's birthdate is mandatory")
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    @NotBlank(message = "A person's ID document is mandatory")
     @Column(name = "id_document")
     private String idDocument;
 
     private Principal principal;
+
+    @OneToOne(mappedBy = "account", cascade = ALL, fetch = LAZY)
+    private Address address;
 
     @Override
     public boolean equals(final Object other) {
