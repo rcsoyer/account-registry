265ee8d0cc4664a0fa1a69a0c78901bedc7b6b75
Rafa Soyer <rcsoyer@gmail.com>
2024-04-20 14:25:38 +0200

    wip - this is not very useful yet. Its just taking notes of some ideas that crossed my mind on reusing Springs generated features and simply providing implementation from within the framework login

---
+++ src/main/java/org/acme/accountregistry/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package org.acme.accountregistry.config;
+
+import org.acme.accountregistry.web.LoginSuccessHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(final HttpSecurity securityBuilder,
+                                                   final LoginSuccessHandler loginSuccessHandler) throws Exception {
+        return securityBuilder
+                 .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                 .formLogin(customizer -> customizer.successHandler(loginSuccessHandler))
+                 .build();
+    }
+
+    @Bean
+    WebSecurityCustomizer httpRequestsPermitAll() {
+        return customizer ->
+                 customizer
+                   .ignoring()
+                   .requestMatchers(GET,
+                                    "/health",
+                                    "/health/liveness",
+                                    "/health/readiness",
+                                    "/info");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    UserDetailsService userDetailsService() {
+        return username -> null;
+    }
+
+}
---
+++ src/main/java/org/acme/accountregistry/web/LoginSuccessHandler.java
@@ -0,0 +1,24 @@
+package org.acme.accountregistry.web;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    public LoginSuccessHandler() {
+        super();
+    }
+
+    @Override
+    public void onAuthenticationSuccess(final HttpServletRequest request,
+                                        final HttpServletResponse response,
+                                        final Authentication authentication) throws IOException {
+        response.getWriter().write("jwt");
+    }
+}
