75941a3ebe72a30659e49e4f3ddb10bad9bbceda
Rafa Soyer <rcsoyer@gmail.com>
2024-04-21 11:15:41 +0200

    add builder with logic

--- src/main/java/org/acme/accountregistry/domain/Address.java
+++ src/main/java/org/acme/accountregistry/domain/Address.java
@@ -6,17 +6,23 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+import static org.apache.commons.lang3.StringUtils.deleteWhitespace;
+import static org.apache.commons.lang3.StringUtils.normalizeSpace;
 
 @Getter
 @Entity
 @ToString
+@NoArgsConstructor(access = PROTECTED)
 public class Address extends AbstractIdEntity {
 
     private String street;
@@ -35,6 +41,19 @@ public class Address extends AbstractIdEntity {
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "id")
     private Account account;
+
+    @Builder
+    private Address(final String street,
+                    final String city,
+                    final String zipCode,
+                    final Country country,
+                    final Account account) {
+        this.street = normalizeSpace(street);
+        this.city = normalizeSpace(city);
+        this.zipCode = deleteWhitespace(zipCode);
+        this.country = country;
+        this.account = account;
+    }
 
     @Getter
     @RequiredArgsConstructor(access = PRIVATE)
