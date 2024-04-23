45957a8de04df171d3e17b5019fc6336f981c588
Rafa Soyer <rcsoyer@gmail.com>
2024-04-23 18:33:44 +0200

    more tests: check validation of country

--- src/test/java/org/acme/accountregistry/domain/AddressTest.java
+++ src/test/java/org/acme/accountregistry/domain/AddressTest.java
@@ -1,27 +1,53 @@
 package org.acme.accountregistry.domain;
 
-import com.neovisionaries.i18n.CountryCode;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import java.util.stream.Stream;
 
+import com.neovisionaries.i18n.CountryCode;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.web.server.ResponseStatusException;
+
+import static com.neovisionaries.i18n.CountryCode.BE;
 import static com.neovisionaries.i18n.CountryCode.NL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AddressTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"NL", "BE"})
-    void testConstructor_whenCountriesAllowedThenSuccess(final String countryCode) {
-        final var address = Address.builder()
-                                   .street("  123  main  st  ")
-                                   .city("  New  York  ")
-                                   .zipCode("  10001 BA ")
-                                   .country(CountryCode.valueOf(countryCode))
-                                   .build();
+    @Nested
+    class TestConstructor {
 
-        assertEquals("123 main st", address.getStreet());
-        assertEquals("New York", address.getCity());
-        assertEquals("10001BA", address.getZipCode());
-        assertEquals(CountryCode.valueOf(countryCode), address.getCountry());
+        @ParameterizedTest
+        @ValueSource(strings = {"NL", "BE"})
+        void testConstructor_whenCountriesAllowedThenSuccess(final String countryCode) {
+            final var address = Address.builder()
+                                       .street("  123  main  st  ")
+                                       .city("  New  York  ")
+                                       .zipCode("  10001 BA ")
+                                       .country(CountryCode.valueOf(countryCode))
+                                       .build();
+
+            assertEquals("123 main st", address.getStreet());
+            assertEquals("New York", address.getCity());
+            assertEquals("10001BA", address.getZipCode());
+            assertEquals(CountryCode.valueOf(countryCode), address.getCountry());
+        }
+
+        @ParameterizedTest
+        @MethodSource("notAllowedCountries")
+        void testConstructor_whenCountriesNotAllowedThenError(final CountryCode countryCode) {
+            assertThrows(ResponseStatusException.class,
+                         () -> Address.builder()
+                                      .country(countryCode)
+                                      .build());
+        }
+
+        static Stream<CountryCode> notAllowedCountries() {
+            return Stream.of(CountryCode.values())
+                         .filter(countryCode -> countryCode != NL && countryCode != BE);
+        }
+
     }
 }
