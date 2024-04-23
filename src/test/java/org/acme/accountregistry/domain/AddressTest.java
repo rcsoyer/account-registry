2c22e1f3b9ffc868fc9ec88adf46f6d155ff8431
Rafa Soyer <rcsoyer@gmail.com>
2024-04-23 18:08:56 +0200

    also test the expected country is being held in the object

--- src/test/java/org/acme/accountregistry/domain/AddressTest.java
+++ src/test/java/org/acme/accountregistry/domain/AddressTest.java
@@ -2,6 +2,7 @@ package org.acme.accountregistry.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static com.neovisionaries.i18n.CountryCode.NL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddressTest {
@@ -12,10 +13,12 @@ class AddressTest {
                                    .street("  123  mAin  st  ")
                                    .city("  new  york  ")
                                    .zipCode("  10001 BA ")
+                                   .country(NL)
                                    .build();
 
         assertEquals("123 Main St", address.getStreet());
         assertEquals("New York", address.getCity());
         assertEquals("10001BA", address.getZipCode());
+        assertEquals(NL, address.getCountry());
     }
 }
