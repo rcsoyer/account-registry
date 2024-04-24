b48b07bd86843429c3623406982632acaa594cd6
Rafa Soyer <rcsoyer@gmail.com>
2024-04-24 09:37:11 +0200

    test the equalsAndHashCode per Iban

--- src/test/java/org/acme/accountregistry/domain/BankAccountTest.java
+++ src/test/java/org/acme/accountregistry/domain/BankAccountTest.java
@@ -3,11 +3,14 @@ package org.acme.accountregistry.domain;
 import java.math.BigDecimal;
 
 import com.neovisionaries.i18n.CountryCode;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.acme.accountregistry.fixtures.DataUtils.account;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class BankAccountTest {
 
@@ -30,4 +33,28 @@ class BankAccountTest {
         assertEquals(accountCountry.getAlpha2(), bankAccount.getIban().getCountryCode().getAlpha2());
     }
 
+    @Nested
+    class TestEqualsAndHashCode {
+
+        @Test
+        void testEqualsAndHashCode_whenSameIbanThenEquals() {
+            final Account accountHolder = account();
+            final var bankAccount1 = new BankAccount(accountHolder, BankAccount.Type.PAYMENTS);
+            final var bankAccount2 = new BankAccount(accountHolder, BankAccount.Type.PAYMENTS);
+            ReflectionTestUtils.setField(bankAccount2, "iban", bankAccount1.getIban());
+
+            assertEquals(bankAccount1, bankAccount2);
+            assertEquals(bankAccount1.hashCode(), bankAccount2.hashCode());
+        }
+
+        @Test
+        void testEqualsAndHashCode_whenDifferentIbanThenNotEquals() {
+            final Account accountHolder = account();
+            final var bankAccount1 = new BankAccount(accountHolder, BankAccount.Type.SAVINGS);
+            final var bankAccount2 = new BankAccount(accountHolder, BankAccount.Type.SAVINGS);
+
+            assertNotEquals(bankAccount1, bankAccount2);
+            assertNotEquals(bankAccount1.hashCode(), bankAccount2.hashCode());
+        }
+    }
 }
