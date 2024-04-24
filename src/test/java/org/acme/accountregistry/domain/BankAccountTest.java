6133c62853938d19d2d9ab020abe68208eafd89b
Rafa Soyer <rcsoyer@gmail.com>
2024-04-24 09:31:39 +0200

    add BankAccount instantiation test to verify the data is being populated as expected

---
+++ src/test/java/org/acme/accountregistry/domain/BankAccountTest.java
@@ -0,0 +1,30 @@
+package org.acme.accountregistry.domain;
+
+import java.math.BigDecimal;
+
+import com.neovisionaries.i18n.CountryCode;
+import org.junit.jupiter.api.Test;
+
+import static org.acme.accountregistry.fixtures.DataUtils.account;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BankAccountTest {
+
+    @Test
+    void testConstructor() {
+        final Account accountHolder = account();
+        final var accountType = BankAccount.Type.PAYMENTS;
+        final var bankAccount = new BankAccount(accountHolder, accountType);
+        final CountryCode accountCountry = accountHolder.getAddress().getCountry();
+
+        assertEquals(accountHolder, bankAccount.getAccountHolder());
+        assertThat(accountHolder.getBankAccounts()).containsExactly(bankAccount);
+        assertEquals(accountType, bankAccount.getType());
+        assertEquals(BigDecimal.ZERO, bankAccount.getBalance());
+
+        assertEquals(accountCountry.getCurrency(), bankAccount.getCurrency());
+        assertEquals(accountCountry.getAlpha2(), bankAccount.getIban().getCountryCode().getAlpha2());
+    }
+
+}
