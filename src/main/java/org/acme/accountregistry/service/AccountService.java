526fc90d78089d0c47c557e1c5f27c218f3aee65
Rafa Soyer <rcsoyer@gmail.com>
2024-04-24 14:02:42 +0200

    setting up the feature of Account registration

--- src/main/java/org/acme/accountregistry/service/AccountService.java
+++ src/main/java/org/acme/accountregistry/service/AccountService.java
@@ -2,7 +2,12 @@ package org.acme.accountregistry.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.acme.accountregistry.domain.Account;
 import org.acme.accountregistry.repository.AccountRepository;
+import org.acme.accountregistry.service.dto.AccountRegisterRequest;
+import org.acme.accountregistry.service.dto.AccountRegisterResponse;
+import org.acme.accountregistry.service.mapper.AccountMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AccountService {
 
-    private final AccountRepository accountRepository;
+    private final AccountRepository repository;
+    private final AccountMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public AccountRegisterResponse createAccount(final AccountRegisterRequest request) {
+        log.debug("Registering a new User's Account with default opening Payments Bank Account");
+        final String password = passwordEncoder.encode(PasswordUtils.generateRandomPassword());
+        final Account account = mapper.toEntity(request, password);
+        return mapper.toResponse(repository.save(account));
+    }
 
 }
--- src/main/java/org/acme/accountregistry/service/mapper/AccountMapper.java
+++ src/main/java/org/acme/accountregistry/service/mapper/AccountMapper.java
@@ -2,6 +2,7 @@ package org.acme.accountregistry.service.mapper;
 
 import org.acme.accountregistry.domain.Account;
 import org.acme.accountregistry.service.dto.AccountRegisterRequest;
+import org.acme.accountregistry.service.dto.AccountRegisterResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,4 +13,8 @@ public interface AccountMapper {
     @Mapping(target = "principal",
              expression = "java(new Principal(request.username(), password))")
     Account toEntity(AccountRegisterRequest request, String password);
+
+    @Mapping(target = "username", source = "account.principal.username")
+    @Mapping(target = "password", source = "account.principal.password")
+    AccountRegisterResponse toResponse(Account account);
 }
