13c609ea38d55b328b16755c9e78772fab26da22
Rafa Soyer <rcsoyer@gmail.com>
2024-04-25 17:24:24 +0200

    disable csrf in order to also allow curl post login calls to the same endpoint called by Spring's formLogin feature. Login success for both cases

--- src/main/java/org/acme/accountregistry/config/WebSecurityConfig.java
+++ src/main/java/org/acme/accountregistry/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package org.acme.accountregistry.config;
 
+import lombok.RequiredArgsConstructor;
 import org.acme.accountregistry.web.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,12 +19,15 @@ import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 class WebSecurityConfig {
 
+    private final LoginSuccessHandler loginSuccessHandler;
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(final HttpSecurity securityBuilder,
-                                                   final LoginSuccessHandler loginSuccessHandler) throws Exception {
+    SecurityFilterChain defaultSecurityFilterChain(final HttpSecurity securityBuilder) throws Exception {
         return securityBuilder
+                 .csrf(AbstractHttpConfigurer::disable)
                  .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
                  .formLogin(customizeLogin(loginSuccessHandler))
                  .build();
