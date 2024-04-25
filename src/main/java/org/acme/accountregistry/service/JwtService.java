0d28e05379bfcd76b866ae6563fcbf613c4d47ec
Rafa Soyer <rcsoyer@gmail.com>
2024-04-25 21:27:55 +0200

    also set up expiration time via env properties

--- src/main/java/org/acme/accountregistry/service/JwtService.java
+++ src/main/java/org/acme/accountregistry/service/JwtService.java
@@ -1,5 +1,8 @@
 package org.acme.accountregistry.service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.UUID;
 import javax.crypto.SecretKey;
@@ -15,23 +18,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
-    public static final MacAlgorithm SIGN_ALG = Jwts.SIG.HS512;
+    private static final MacAlgorithm SIGN_ALG = Jwts.SIG.HS512;
 
     private final SecretKey secretKey;
+    private final long jwtExpirationHours;
 
     public JwtService(@Value("${security.jwt.secret-key}") final String secretKey,
+                      @Value("${security.jwt.expiration-hours}") final int expirationHours,
                       final Converter<String, SecretKey> secretKeyConverter) {
         this.secretKey = secretKeyConverter.convert(secretKey);
+        this.jwtExpirationHours = expirationHours;
     }
 
     public String generateJwt(final Authentication authentication) {
         final var principal = (UserDetails) authentication.getPrincipal();
-        final long jwtExpirationMs = 86400000;
+        final Instant jwtExpiration = LocalDateTime.now().plusHours(jwtExpirationHours).toInstant(ZoneOffset.UTC);
         return Jwts.builder()
                    .id(UUID.randomUUID().toString())
                    .subject((principal.getUsername()))
                    .issuedAt(new Date())
-                   .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                   .expiration(Date.from(jwtExpiration))
                    .signWith(secretKey, SIGN_ALG)
                    .issuer("acme.org")
                    .claim("AUTHORITIES", principal.getAuthorities())
