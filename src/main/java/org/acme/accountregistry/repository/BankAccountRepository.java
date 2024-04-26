0991f4cc4347ce9de937cd82c14090024dbed1af
Rafa Soyer <rcsoyer@gmail.com>
2024-04-26 19:50:20 +0200

    refactorings: if it's called more than once, the column will not be included in the sql update

--- src/main/java/org/acme/accountregistry/repository/BankAccountRepository.java
+++ src/main/java/org/acme/accountregistry/repository/BankAccountRepository.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 
-    @Query("SELECT bankAccount.iban AS accountNumber," +
-             "bankAccount.type AS accountType," +
-             "bankAccount.balance AS balance," +
+    @Query("SELECT bankAccount.iban AS accountNumber, " +
+             "bankAccount.type AS accountType, " +
+             "bankAccount.balance AS balance, " +
              "bankAccount.currency AS currency " +
              "FROM BankAccount bankAccount " +
              "JOIN Account accountHolder ON bankAccount.accountHolder = accountHolder " +
