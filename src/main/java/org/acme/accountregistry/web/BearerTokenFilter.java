0ea7a57252d85525a3f60e1a8a6db4f57dcb72f3
Rafa Soyer <rcsoyer@gmail.com>
2024-04-26 11:22:58 +0200

    the class can be package-private. It is not needed to expose this class outside since it is a OncePerRequestFilter and a @Component, Spring is able to add this BearerTokenFilter as a filter, to the security chain, automatically

--- src/main/java/org/acme/accountregistry/web/BearerTokenFilter.java
+++ src/main/java/org/acme/accountregistry/web/BearerTokenFilter.java
@@ -17,7 +17,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @RequiredArgsConstructor
-public class BearerTokenFilter extends OncePerRequestFilter {
+class BearerTokenFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
 
