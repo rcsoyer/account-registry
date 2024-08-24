4e031ee453e516090877e015a1ae436598eb1a9a
Rafael Soyer <rcsoyer@gmail.com>
2024-08-24 19:27:28 +0200

    on login success sets the authenticatedUser to the security context

--- src/main/java/org/acme/accountregistry/application/events/AuthenticationEventHandler.java
+++ src/main/java/org/acme/accountregistry/application/events/AuthenticationEventHandler.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,9 +15,9 @@ class AuthenticationEventHandler {
 
     @EventListener
     void onSuccess(final AuthenticationSuccessEvent loginSuccessEvent) {
-        log.debug(
-                "Successfully authenticated user {}",
-                loginSuccessEvent.getAuthentication().getName());
+        final Authentication authentication = loginSuccessEvent.getAuthentication();
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.debug("Successfully authenticated user {}", authentication.getName());
     }
 
     @EventListener
