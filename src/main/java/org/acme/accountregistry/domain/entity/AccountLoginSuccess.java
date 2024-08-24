a1fdafa6352b0a383683ebfaf333fdf8cb2ece60
Rafael Soyer <rcsoyer@gmail.com>
2024-08-24 19:27:06 +0200

    on login success sets the authenticatedUser to the security context

--- src/main/java/org/acme/accountregistry/domain/entity/AccountLoginSuccess.java
+++ src/main/java/org/acme/accountregistry/domain/entity/AccountLoginSuccess.java
@@ -2,19 +2,38 @@ package org.acme.accountregistry.domain.entity;
 
 import static lombok.AccessLevel.PROTECTED;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import java.time.Instant;
+
 @Getter
-@Entity
+// @Entity
 @NoArgsConstructor(access = PROTECTED)
 public class AccountLoginSuccess extends AbstractImmutableEntity {
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "account_id", updatable = false)
     private Account account;
+
+    private Instant loginEventTimestamp;
+
+    @Embedded private WebAuthenticationDetails authenticationDetails;
+
+    @Builder
+    private AccountLoginSuccess(
+            final Account account,
+            final long loginEventTimestamp,
+            final WebAuthenticationDetails authenticationDetails) {
+        this.account = account;
+        this.loginEventTimestamp = Instant.ofEpochMilli(loginEventTimestamp);
+        this.authenticationDetails = authenticationDetails;
+    }
 }
