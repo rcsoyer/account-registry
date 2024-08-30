9a1c575f743b3141978fff9de1b28446dabfc1fa
Rafael Soyer <rcsoyer@gmail.com>
2024-08-30 20:43:44 +0200

    refactoring - simplify and allow for more fields to taken from spring's authenticationSuccessEvent. Besides, this way is also possible to create the event through the type of success or failure

--- src/main/java/org/acme/accountregistry/domain/service/mapper/AccountAuthenticationEventMapper.java
+++ src/main/java/org/acme/accountregistry/domain/service/mapper/AccountAuthenticationEventMapper.java
@@ -2,23 +2,12 @@ package org.acme.accountregistry.domain.service.mapper;
 
 import org.acme.accountregistry.domain.entity.Account;
 import org.acme.accountregistry.domain.entity.AccountAuthenticationEvent;
-import org.acme.accountregistry.domain.entity.AccountAuthenticationEvent.AuthenticationEventType;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
-@Mapper(
-        uses = {AccountMapper.class},
-        imports = {WebAuthenticationDetails.class, AuthenticationEventType.class})
+@Mapper(uses = {AccountMapper.class})
 public interface AccountAuthenticationEventMapper {
 
-    @Mapping(
-            target = "remoteAddress",
-            expression =
-                    "java(((WebAuthenticationDetails) event.getAuthentication().getDetails()).getRemoteAddress())")
-    @Mapping(target = "authenticationTimestamp", expression = "java(event.getTimestamp())")
-    @Mapping(target = "eventType", expression = "java(AuthenticationEventType.SUCCESS)")
     AccountAuthenticationEvent sucessAccountAuthenticationEvent(
-            AuthenticationSuccessEvent event, Account account);
+            Account account, AuthenticationSuccessEvent event);
 }
