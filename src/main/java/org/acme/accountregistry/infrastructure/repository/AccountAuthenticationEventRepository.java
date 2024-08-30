ccee9739d3f88b7635283116f894f38822b36dc9
Rafael Soyer <rcsoyer@gmail.com>
2024-08-30 23:39:40 +0200

    use the new scheme formatting

--- src/main/java/org/acme/accountregistry/infrastructure/repository/AccountAuthenticationEventRepository.java
+++ src/main/java/org/acme/accountregistry/infrastructure/repository/AccountAuthenticationEventRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountAuthenticationEventRepository
-        extends JpaRepository<AccountAuthenticationEvent, Long> {}
+  extends JpaRepository<AccountAuthenticationEvent, Long> {
+}
