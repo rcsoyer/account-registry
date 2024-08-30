7b95acd32e1b048bb17232ebb412180e1ee1bcd3
Rafael Soyer <rcsoyer@gmail.com>
2024-08-30 23:36:00 +0200

    use the new scheme formatting

--- src/main/java/org/acme/accountregistry/domain/service/AccountAuthenticationEventService.java
+++ src/main/java/org/acme/accountregistry/domain/service/AccountAuthenticationEventService.java
@@ -1,16 +1,14 @@
 package org.acme.accountregistry.domain.service;
 
+import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.acme.accountregistry.domain.entity.AccountAuthenticationEvent;
 import org.acme.accountregistry.infrastructure.repository.AccountAuthenticationEventRepository;
 import org.acme.accountregistry.infrastructure.repository.AccountRepository;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.function.Supplier;
 
 @Slf4j
 @Service
@@ -22,21 +20,21 @@ public class AccountAuthenticationEventService {
     private final AccountRepository accountRepository;
 
     public void createSuccessEvent(final AuthenticationSuccessEvent event) {
+        log.debug("Persisting Account Authentication success event published by spring's security context");
         final String username = event.getAuthentication().getName();
         accountRepository
-                .findAccountByPrincipalUsername(username)
-                .map(account -> new AccountAuthenticationEvent(account, event))
-                .map(eventRepository::save)
-                .orElseThrow(errorAccountNotFound(username));
+          .findAccountByPrincipalUsername(username)
+          .map(account -> new AccountAuthenticationEvent(account, event))
+          .map(eventRepository::save)
+          .orElseThrow(errorAccountNotFound(username));
     }
 
     private Supplier<IllegalStateException> errorAccountNotFound(final String username) {
         return () -> {
-            log.error(
-                    "Unexpected error that shouldn't happen. "
-                            + "The Spring's Security context published an authentication success event "
-                            + "that the username doesn't exists in the system. username={}",
-                    username);
+            log.error("Unexpected error that shouldn't happen. "
+                        + "The Spring's Security context published an authentication success event "
+                        + "that the username doesn't exists in the system. username={}",
+              username);
             return new IllegalStateException("Account not found");
         };
     }
