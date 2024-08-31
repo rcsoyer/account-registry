82780c4cded301fd0701f72478007313c20893df
Rafael Soyer <rcsoyer@gmail.com>
2024-08-31 15:58:34 +0200

    remove not null constraint. For Authentication failures those fields may be null

--- src/main/java/org/acme/accountregistry/domain/entity/AbstractImmutableEntity.java
+++ src/main/java/org/acme/accountregistry/domain/entity/AbstractImmutableEntity.java
@@ -1,22 +1,20 @@
 package org.acme.accountregistry.domain.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
-
 import static lombok.AccessLevel.PROTECTED;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.Instant;
 
 @Getter
 @ToString
@@ -29,13 +27,13 @@ abstract class AbstractImmutableEntity {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @NotNull
     @CreatedDate
     @Column(name = "date_created", updatable = false)
     private Instant createdAt;
 
-    /** username that created the entity */
-    @NotBlank
+    /**
+     * username that created the entity
+     */
     @CreatedBy
     @Column(name = "created_by", updatable = false)
     private String createdBy;
