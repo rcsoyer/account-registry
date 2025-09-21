54f76ecc89f7a499876c60aa394f4539c02f2dfb
rcsoyer <rcsoyer@gmail>
2025-09-21 09:05:38 +0200

    create Money representation, for better management of money amounts with expected bigdecimal scale

---
+++ src/main/java/org/acme/accountregistry/domain/entity/Money.java
@@ -0,0 +1,51 @@
+package org.acme.accountregistry.domain.entity;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.server.ResponseStatusException;
+
+import static lombok.AccessLevel.PROTECTED;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Getter
+@ToString
+@Embeddable
+@NoArgsConstructor(access = PROTECTED)
+public class Money {
+
+    @NotNull(message = "The amount of money is mandatory")
+    private BigDecimal amount;
+
+    @NotNull(message = "The currency of the money is mandatory")
+    private Currency currency;
+
+    public Money(final BigDecimal amount, final Currency currency) {
+        this.amount = amount.setScale(2, RoundingMode.HALF_UP);
+        this.currency = currency;
+    }
+
+    public void add(final Money money) {
+        validateSameCurrency(money);
+        amount = amount.add(money.getAmount());
+    }
+
+    public void subtract(final Money money) {
+        validateSameCurrency(money);
+        amount = amount.subtract(money.getAmount());
+    }
+
+    private void validateSameCurrency(final Money otherMoney) {
+        if (!getCurrency().equals(otherMoney.getCurrency())) {
+            throw new ResponseStatusException(
+              BAD_REQUEST,
+              "Its only supported to operate Money with same currency"
+            );
+        }
+    }
+}
