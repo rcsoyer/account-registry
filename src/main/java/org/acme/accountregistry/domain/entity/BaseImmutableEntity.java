4ee8f0deec186d33e1aec1b593236a31335ea699
rcsoyer <rcsoyer@gmail>
2025-09-21 10:59:23 +0200

    wip - defining the transfer recording

--- src/main/java/org/acme/accountregistry/domain/entity/BaseImmutableEntity.java
+++ src/main/java/org/acme/accountregistry/domain/entity/BaseImmutableEntity.java
@@ -1,20 +1,22 @@
 package org.acme.accountregistry.domain.entity;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+import static org.apache.commons.lang3.ObjectUtils.anyNull;
 
 @Getter
 @ToString
@@ -37,4 +39,24 @@ abstract class BaseImmutableEntity {
     @CreatedBy
     @Column(name = "created_by", updatable = false)
     private String createdBy;
+
+    @Override
+    public boolean equals(final Object another) {
+        if (another == null || getClass() != another.getClass()) {
+            return false;
+        }
+
+        final BaseImmutableEntity that = (BaseImmutableEntity) another;
+
+        if (anyNull(getId(), that.getId())) {
+            return false;
+        }
+
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 }
