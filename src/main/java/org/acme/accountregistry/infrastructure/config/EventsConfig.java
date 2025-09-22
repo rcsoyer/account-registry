c3157a9e67ec0bef2d80557d81fc8411dbe7f60c
rcsoyer <rcsoyer@gmail>
2025-09-22 12:26:03 +0200

    simple refactoring - use spring infrascture, that already set up an Executor of virtual threads

--- src/main/java/org/acme/accountregistry/infrastructure/config/EventsConfig.java
+++ src/main/java/org/acme/accountregistry/infrastructure/config/EventsConfig.java
@@ -1,15 +1,20 @@
 package org.acme.accountregistry.infrastructure.config;
 
-import org.springframework.boot.autoconfigure.context.LifecycleProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 
+/**
+ * Events published by the Spring context
+ *
+ * @see ApplicationEventPublisher
+ */
 @Configuration
 class EventsConfig {
 
@@ -17,20 +22,19 @@ class EventsConfig {
      * Set up for the Spring framework to publish and handle events asynchronously
      */
     @Bean
-    ApplicationEventMulticaster applicationEventMulticaster(final LifecycleProperties lifecycleProperties) {
-        final var asyncTaskExecutor = new SimpleAsyncTaskExecutor();
-        asyncTaskExecutor.setVirtualThreads(true);
-        final long taskTerminationTimeOut = lifecycleProperties.getTimeoutPerShutdownPhase().toMillis();
-        asyncTaskExecutor.setTaskTerminationTimeout(taskTerminationTimeOut);
-
+    ApplicationEventMulticaster applicationEventMulticaster(
+      @Qualifier("applicationTaskExecutor") final AsyncTaskExecutor virtualThreadAsyncTaskExecutor) {
         final var eventMulticaster = new SimpleApplicationEventMulticaster();
-        eventMulticaster.setTaskExecutor(asyncTaskExecutor);
-
+        eventMulticaster.setTaskExecutor(virtualThreadAsyncTaskExecutor);
         return eventMulticaster;
     }
 
+    /**
+     * Set up the authentication event publisher with the current application event publisher
+     */
     @Bean
-    AuthenticationEventPublisher authenticationEventPublisher(final ApplicationEventPublisher applicationEventPublisher) {
+    AuthenticationEventPublisher authenticationEventPublisher(
+      final ApplicationEventPublisher applicationEventPublisher) {
         return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
     }
 }
