118171660cfd44525ddf65515d349998d6faad18
rcsoyer <rcsoyer@gmail>
2025-09-27 19:34:13 +0200

    simple refactoring - add javadocs for future reference

---
+++ src/main/java/org/acme/accountregistry/domain/dto/command/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Data transfer objects for commands.
+ * <br/> They are used to write data into to the application.
+ * <br/> They are immutable and therefore thread-safe.
+ * <br/> They are expected to implement the {@link java.io.Serializable} interface, because they go "over the wire".
+ */
+
+package org.acme.accountregistry.domain.dto.command;
