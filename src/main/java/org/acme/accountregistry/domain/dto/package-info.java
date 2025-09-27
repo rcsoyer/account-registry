de2983953c36e7ea5b10425207ae6e121f9c6357
rcsoyer <rcsoyer@gmail>
2025-09-27 19:32:35 +0200

    simple refactoring - add javadocs for future reference

---
+++ src/main/java/org/acme/accountregistry/domain/dto/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Data Transfer Objects.
+ * <br/> DTOs are used to transfer data between the application and the clients.
+ * <br/> They are immutable and therefore thread-safe.
+ * <br/> They are expected to implement the {@link java.io.Serializable} interface, because they go "over the wire".
+ */
+package org.acme.accountregistry.domain.dto;
